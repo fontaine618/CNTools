dda54f918d31d4a084324f6fa145e91eed4b088a
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-11-07 18:01:51 +0000

    supply lib.loc argument to library.dynam() (now required)
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNTools@60096 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(lib, pkgname){
    require("methods", quietly = TRUE) || stop("methods package not found")
    require("tools", quietly = TRUE) || stop("methods package not found")
-   library.dynam( "CNTools", package = "CNTools") 
+   library.dynam( "CNTools", package = "CNTools", lib.loc=NULL) 
 }
 
 .onUnload <- function( libpath ) {
